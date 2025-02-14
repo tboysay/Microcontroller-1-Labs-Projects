605e857af36c7bcb44d4b89c5dd518984b68ef6d
tboysay <83206110+tboysay@users.noreply.github.com>
2025-02-14 13:11:35 +0000

    9.9 Q1A

--- MPLab Working Template/example.s
+++ MPLab Working Template/example.s
@@ -1,32 +1,27 @@
-; Program to move 3 values to 3 memory 
-; Add the global options "-Wl,-presetVector=0h, -Wl,-pstart=200h" in MPLAB before building this
+; Name: Timi Ayo-Yusuf
+; Student ID: 11591999    
+    
     
 processor 18F8722
-radix   dec ; use decimal numbers
+radix dec 
     
-CONFIG  OSC = HS  ; use the high speed external crystal on the PCB
-CONFIG  WDT = OFF ; turn off the watchdog timer
-CONFIG  LVP = OFF ; turn off low voltage mode
-
-;Include useful processor-specific definitions
-#include <xc.inc>      
+CONFIG OSC = HS
+CONFIG WDT = OFF
+CONFIG LVP = OFF
+    
+#include <xc.inc.>
 
 PSECT resetVector, class=CODE, reloc=2
 resetVector:
-    goto start ; On reset, jump to our program start location
-
-; Our program start point
+    goto start
+    
 PSECT start, class=CODE, reloc=2
-start: 
-    movlw 22
-    movwf 10
-    movlw 00101001B
-    movwf 11
-    movlw 0x15
-    movwf 12
+start:
+    movlw 8 ; puts 8 in WREG
+    movf 15h, W ; copies WREG into location 15h
 loop:
-    bra loop ; endless loop
-    end
+    bra loop
+    end 
 
 
 
